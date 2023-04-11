197e29f678238943ca1cc5b7342af90e7bf2a10d
Vladimir <scofari99@gmail.com>
2023-04-11 23:07:46 +0300

    bugfix/ added transition for sidebar

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -8,34 +8,34 @@ import { useAppDispatch } from "../../redux/store";
 import { setDropDownMenu } from "../../redux/appSlice";
 
 const Search: FC = () => {
-	const [toggleSearch, setToggleSearch] = useState(false);
-	const ref = useClickOutside(() => setToggleSearch(false));
-	const dispatch = useAppDispatch();
+    const [toggleSearch, setToggleSearch] = useState(false);
+    const ref = useClickOutside(() => setToggleSearch(false));
+    const dispatch = useAppDispatch();
 
-	useEffect(() => {
-		dispatch(setDropDownMenu(toggleSearch));
-	}, [toggleSearch]);
+    useEffect(() => {
+        dispatch(setDropDownMenu(toggleSearch));
+    }, [toggleSearch]);
 
-	return (
-		<div ref={ref} className={`${styles.search} ${styles.hide}`}>
-			{toggleSearch && (
-				<div className={styles.wrapper}>
-					<h2>Search</h2>
-					<SearchInput />
-					<div className={styles.searchContentBlock}>
-						<div className={styles.searchContent}>
-							<h4>Recent</h4>
-							<span>No recent searches</span>
-						</div>
-					</div>
-				</div>
-			)}
+    return (
+        <div ref={ref} className={`${styles.search} ${styles.hide}`}>
+            {toggleSearch && (
+                <div className={styles.wrapper}>
+                    <h2>Search</h2>
+                    <SearchInput />
+                    <div className={styles.searchContentBlock}>
+                        <div className={styles.searchContent}>
+                            <h4>Recent</h4>
+                            <span>No recent searches</span>
+                        </div>
+                    </div>
+                </div>
+            )}
 
-			<div onClick={() => setToggleSearch((prev) => !prev)}>
-				<NavigationItem icon={RiSearchLine} title="Search" />
-			</div>
-		</div>
-	);
+            <div onClick={() => setToggleSearch((prev) => !prev)}>
+                <NavigationItem icon={RiSearchLine} title="Search" />
+            </div>
+        </div>
+    );
 };
 
 export default Search;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -8,24 +8,30 @@ import styles from "./Sidebar.module.scss";
 import { Link } from "react-router-dom";
 
 const Sidebar: FC = () => {
-	const dropDownMenu = useSelector(
-		(state: RootState) => state.app.dropDownMenu
-	);
+    const dropDownMenu = useSelector(
+        (state: RootState) => state.app.dropDownMenu
+    );
 
-	return (
-		<div className={styles.sidebar}>
-			{dropDownMenu ? (
-				<div className={styles.logoSmall}>
-					<Link to="/">
-						<BsInstagram />
-					</Link>
-				</div>
-			) : (
-				<LogoText />
-			)}
-			<NavigationMenu />
-		</div>
-	);
+    return (
+        <div
+            className={`${styles.sidebar} ${
+                dropDownMenu ? styles.sidebarSmall : ""
+            }`}
+        >
+            {dropDownMenu ? (
+                <div className={styles.logoSmall}>
+                    <Link to="/">
+                        <BsInstagram />
+                    </Link>
+                </div>
+            ) : (
+                <div className={styles.logoSmall}>
+                    <LogoText />
+                </div>
+            )}
+            <NavigationMenu />
+        </div>
+    );
 };
 
 export default Sidebar;
